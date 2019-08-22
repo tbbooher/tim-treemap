cbfa639631cf82575f95c7633e97f30e8ce8f522
tim booher <tim@theboohers.org>
2019-08-22 12:22:53 -0400

    First commit

---
+++ src/index.js
@@ -0,0 +1,164 @@
+const dscc = require("@google/dscc");
+const local = require("./localMessage.js");
+const d3 = require("d3");
+
+// change this to "true" for local development
+// change this to "false" before deploying
+// 
+export const LOCAL = false;
+
+function draw(message) {
+    // let's get our data in a good format
+    var data = message.tables.DEFAULT;
+
+    var tabularData = data.map(function(row) {
+        var accounts = {};
+        accounts['account'] = row.account[0];
+        accounts['market_value'] = row.account_value[0];
+        accounts['percent_change'] = row.percent_change[0];
+        accounts['investment'] = row.investment[0];
+        return accounts;
+    });
+
+    var nestedData = d3
+        .nest()
+        .key(function(d) {
+            return d['account'];
+        })
+        .key(function(d) {
+            return d['investment'];
+        })
+        .rollup(function(leaves) {
+            return {
+                market_value: d3.sum(leaves, function(d) { return +d['market_value']; }),
+                percent_change: d3.sum(leaves, function(d) { return +d['percent_change']; })
+            }
+        })
+        .entries(tabularData);
+
+    const root = d3.hierarchy({ values: nestedData }, (row) => {
+        return row.values || null;
+    });
+
+    root.sum((d) => {
+        return d.value && d.value.market_value || 0;
+    });
+
+    render(root, message.style);
+};
+
+function render(root, style) {
+    // remove the canvas if it exists
+    d3.select('body')
+        .selectAll('svg')
+        .remove();
+
+    var max_value = d3.max(root.data['values'], function(d) { return +d.values[0].value.percent_change; });
+    var min_value = d3.min(root.data['values'], function(d) { return +d.values[0].value.percent_change; });
+
+    var myColor = d3
+        .scaleLinear()
+        .domain([min_value, max_value])
+        .range(["red", "green"]);
+
+    // set the dimensions and margins of the graph
+    var margin = { top: 10, right: 10, bottom: 10, left: 10 },
+        width = dscc.getWidth() - margin.left - margin.right,
+        height = 445 - margin.top - margin.bottom;
+
+    var div = d3
+        .select('body')
+        .append('div')
+        .attr('id', 'my_dataviz');
+
+    var svg = div
+        .append('svg')
+        .attr('width', width - margin.left)
+        .attr('height', height - margin.top)
+        .append('g')
+        .attr('transform', "translate(" + margin.left + "," + margin.top + ")");
+
+    var treemap = d3
+        .treemap()
+        .size([
+            width - margin.left - margin.right,
+            height - margin.top - margin.bottom,
+        ])
+        // .paddingOuter(10)
+        .paddingTop(28)
+        .paddingRight(7)
+        .paddingInner(3)
+        (root)
+
+    svg
+        .selectAll("rect")
+        .data(root.leaves())
+        .enter()
+        .append("rect")
+        .attr('x', function(d) { return d.x0; })
+        .attr('y', function(d) { return d.y0; })
+        .attr('width', function(d) { return d.x1 - d.x0; })
+        .attr('height', function(d) { return d.y1 - d.y0; })
+        .style("stroke", "black")
+        .style("fill", function(d) {
+            var colorVal = d.data.value && d.data.value.percent_change;
+            if (colorVal == 0) {
+                return 'grey';
+            } else {
+                return myColor(colorVal);
+            }
+        });
+
+    // and to add the text labels
+    svg
+        .selectAll("text")
+        .data(root.leaves())
+        .enter()
+        .append("text")
+        .attr("x", function(d) { return d.x0 + 5 }) // +10 to adjust position (more right)
+        .attr("y", function(d) { return d.y0 + 20 }) // +20 to adjust position (lower)
+        .text(function(d) {
+            var width_percent = (d.x1 - d.x0) / width;
+            if ((d.data.key == 'null') || (width_percent < 0.06)) {
+                return '';
+            } else {
+                return d.data.key;
+            }
+        })
+        .attr("fill", "white")
+
+    // and to add the values in
+    svg
+        .selectAll("vals")
+        .data(root.leaves())
+        .enter()
+        .append("text")
+        .attr("x", function(d) { return d.x0 + 5 }) // +10 to adjust position (more right)
+        .attr("y", function(d) { return d.y0 + 35 }) // +20 to adjust position (lower)
+        .text(function(d) {
+            var val = Math.round(parseFloat(d.data.value.market_value) / 1000);
+            return val;
+        })
+        .attr("font-size", "11px")
+        .attr("fill", "white");
+
+    svg
+        .selectAll("titles")
+        .data(root.descendants().filter(function(d) { return d.depth == 1 }))
+        .enter()
+        .append("text")
+        .attr("x", function(d) { return d.x0 })
+        .attr("y", function(d) { return d.y0 + 21 })
+        .text(function(d) {
+            return d.data.key;
+        })
+        .attr("font-size", "19px");
+
+}
+
+// renders locally
+if (LOCAL) {
+    draw(local.message);
+} else {
+    dscc.subscribeToData(draw, { transform: dscc.objectTransform });
+}
---
+++ src/localMessage.js
@@ -0,0 +1,541 @@
+export const message = {
+    "tables": {
+        "DEFAULT": [{
+                "account": [
+                    "Evil Corp"
+                ],
+                "investment": [
+                    null
+                ],
+                "account_value": [
+                    1000
+                ],
+                "percent_change": [
+                    0
+                ]
+            },
+            {
+                "account": [
+                    "Wayne Enterprises"
+                ],
+                "investment": [
+                    "CL"
+                ],
+                "account_value": [
+                    2000
+                ],
+                "percent_change": [
+                    0.011717673007995704
+                ]
+            },
+            {
+                "account": [
+                    "Initech"
+                ],
+                "investment": [
+                    "S Fund"
+                ],
+                "account_value": [
+                    500
+                ],
+                "percent_change": [
+                    -0.015199710967904028
+                ]
+            },
+            {
+                "account": [
+                    "ACME Corp"
+                ],
+                "investment": [
+                    "CWGIX"
+                ],
+                "account_value": [
+                    200
+                ],
+                "percent_change": [
+                    -0.0018860016764460062
+                ]
+            },
+            {
+                "account": [
+                    "ACME Corp"
+                ],
+                "investment": [
+                    "CWIAX"
+                ],
+                "account_value": [
+                    300
+                ],
+                "percent_change": [
+                    -0.0018931426167438665
+                ]
+            },
+            {
+                "account": [
+                    "ACME Corp"
+                ],
+                "investment": [
+                    "FTC"
+                ],
+                "account_value": [
+                    100
+                ],
+                "percent_change": [
+                    -0.0036806342015855763
+                ]
+            },
+            {
+                "account": [
+                    "ACME Corp"
+                ],
+                "investment": [
+                    "BDPS"
+                ],
+                "account_value": [
+                    100
+                ],
+                "percent_change": [
+                    0
+                ]
+            },
+            {
+                "account": [
+                    "ACME Corp"
+                ],
+                "investment": [
+                    "ANWPX"
+                ],
+                "account_value": [
+                    125
+                ],
+                "percent_change": [
+                    -0.002956561291789915
+                ]
+            },
+            {
+                "account": [
+                    "ACME Corp"
+                ],
+                "investment": [
+                    "FDT"
+                ],
+                "account_value": [
+                    232
+                ],
+                "percent_change": [
+                    -0.010729187363401534
+                ]
+            },
+            {
+                "account": [
+                    "ACME Corp"
+                ],
+                "investment": [
+                    "FEM"
+                ],
+                "account_value": [
+                    107
+                ],
+                "percent_change": [
+                    -0.0008665511265166
+                ]
+            },
+            {
+                "account": [
+                    "ACME Corp"
+                ],
+                "investment": [
+                    "FTA"
+                ],
+                "account_value": [
+                    165.5
+                ],
+                "percent_change": [
+                    -0.018326206475259593
+                ]
+            },
+            {
+                "account": [
+                    "ACME Corp"
+                ],
+                "investment": [
+                    "PG"
+                ],
+                "account_value": [
+                    99.9
+                ],
+                "percent_change": [
+                    0.013877207737594665
+                ]
+            },
+            {
+                "account": [
+                    "ACME Corp"
+                ],
+                "investment": [
+                    "FYX"
+                ],
+                "account_value": [
+                    78.62
+                ],
+                "percent_change": [
+                    -0.006516892471274145
+                ]
+            },
+            {
+                "account": [
+                    "ACME Corp"
+                ],
+                "investment": [
+                    "PFE"
+                ],
+                "account_value": [
+                    234.56
+                ],
+                "percent_change": [
+                    -0.017047096214966872
+                ]
+            },
+            {
+                "account": [
+                    "ACME Corp"
+                ],
+                "investment": [
+                    "AFAXX"
+                ],
+                "account_value": [
+                    99.87
+                ],
+                "percent_change": [
+                    0
+                ]
+            },
+            {
+                "account": [
+                    "ACME Corp"
+                ],
+                "investment": [
+                    "FNX"
+                ],
+                "account_value": [
+                    42.23
+                ],
+                "percent_change": [
+                    -0.013217866909753944
+                ]
+            },
+            {
+                "account": [
+                    "ACME Corp"
+                ],
+                "investment": [
+                    "SMCWX"
+                ],
+                "account_value": [
+                    185.2
+                ],
+                "percent_change": [
+                    -0.00759081872401955
+                ]
+            },
+            {
+                "account": [
+                    "ACME Corp"
+                ],
+                "investment": [
+                    "PM"
+                ],
+                "account_value": [
+                    67.67
+                ],
+                "percent_change": [
+                    -0.011445783132530155
+                ]
+            },
+            {
+                "account": [
+                    "ACME Corp"
+                ],
+                "investment": [
+                    "BGY"
+                ],
+                "account_value": [
+                    125
+                ],
+                "percent_change": [
+                    -0.009225092250922476
+                ]
+            },
+            {
+                "account": [
+                    "ACME Corp"
+                ],
+                "investment": [
+                    "XLU"
+                ],
+                "account_value": [
+                    133
+                ],
+                "percent_change": [
+                    0.01426949894600296
+                ]
+            },
+            {
+                "account": [
+                    "ACME Corp"
+                ],
+                "investment": [
+                    "SCZ"
+                ],
+                "account_value": [
+                    162
+                ],
+                "percent_change": [
+                    -0.00949251551661183
+                ]
+            },
+            {
+                "account": [
+                    "ACME Corp"
+                ],
+                "investment": [
+                    "MO"
+                ],
+                "account_value": [
+                    122
+                ],
+                "percent_change": [
+                    -0.0162937214859874
+                ]
+            },
+            {
+                "account": [
+                    "ACME Corp"
+                ],
+                "investment": [
+                    "DBC"
+                ],
+                "account_value": [
+                    45
+                ],
+                "percent_change": [
+                    -0.01673360107095047
+                ]
+            },
+            {
+                "account": [
+                    "ACME Corp"
+                ],
+                "investment": [
+                    "PRRCX"
+                ],
+                "account_value": [
+                    67.2
+                ],
+                "percent_change": [
+                    0.002613240418118566
+                ]
+            },
+            {
+                "account": [
+                    "ACME Corp"
+                ],
+                "investment": [
+                    "CCWAX"
+                ],
+                "account_value": [
+                    19.99
+                ],
+                "percent_change": [
+                    0
+                ]
+            },
+            {
+                "account": [
+                    "ACME Corp"
+                ],
+                "investment": [
+                    "CWICX"
+                ],
+                "account_value": [
+                    22.32
+                ],
+                "percent_change": [
+                    -0.001901140684410568
+                ]
+            }
+        ]
+    },
+    "fields": {
+        "account": [{
+            "id": "qt_dcb3ze9c0b",
+            "name": "Account",
+            "type": "TEXT",
+            "concept": "DIMENSION"
+        }],
+        "investment": [{
+            "id": "qt_hdgqve9c0b",
+            "name": "Symbol",
+            "type": "TEXT",
+            "concept": "DIMENSION"
+        }],
+        "account_value": [{
+            "id": "qt_miw58e9c0b",
+            "name": "Market Value ($)",
+            "type": "NUMBER",
+            "concept": "METRIC"
+        }],
+        "percent_change": [{
+            "id": "qt_3mtfif9c0b",
+            "name": "Percent Change",
+            "type": "PERCENT",
+            "concept": "METRIC"
+        }]
+    },
+    "style": {
+        showL1Labels: {
+            value: true,
+            defaultValue: false,
+        },
+        showL2Labels: {
+            value: true,
+            defaultValue: false,
+        },
+        textXOffset: {
+            value: 1,
+            defaultValue: 0,
+        },
+        textYOffset: {
+            value: 1,
+            defaultValue: 0,
+        },
+        fontSize: {
+            value: 20,
+            defaultValue: 10,
+        },
+        fontColor: {
+            value: {
+                color: 'yellow',
+            },
+            defaultValue: '#000000',
+        },
+        fontFamily: {
+            value: 'Arial',
+            defaultValue: 'Arial',
+        },
+    },
+    "theme": {
+        "themeFillColor": {
+            "color": "#fff",
+            "opacity": 1
+        },
+        "themeFontColor": {
+            "color": "#616161",
+            "opacity": 1
+        },
+        "themeFontFamily": "Roboto",
+        "themeAccentFillColor": {
+            "color": "#4285F4",
+            "opacity": 1
+        },
+        "themeAccentFontColor": {
+            "color": "#EFEFEF",
+            "opacity": 1
+        },
+        "themeAccentFontFamily": "Roboto",
+        "themeSeriesColor": [{
+                "color": "#4285F4",
+                "opacity": 1
+            },
+            {
+                "color": "#DB4437",
+                "opacity": 1
+            },
+            {
+                "color": "#F4B400",
+                "opacity": 1
+            },
+            {
+                "color": "#0F9D58",
+                "opacity": 1
+            },
+            {
+                "color": "#AB47BC",
+                "opacity": 1
+            },
+            {
+                "color": "#00ACC1",
+                "opacity": 1
+            },
+            {
+                "color": "#FF7043",
+                "opacity": 1
+            },
+            {
+                "color": "#9E9D24",
+                "opacity": 1
+            },
+            {
+                "color": "#5C6BC0",
+                "opacity": 1
+            },
+            {
+                "color": "#F06292",
+                "opacity": 1
+            },
+            {
+                "color": "#00796b",
+                "opacity": 1
+            },
+            {
+                "color": "#c2185b",
+                "opacity": 1
+            },
+            {
+                "color": "#7e57c2",
+                "opacity": 1
+            },
+            {
+                "color": "#03a9f4",
+                "opacity": 1
+            },
+            {
+                "color": "#8bc34a",
+                "opacity": 1
+            },
+            {
+                "color": "#fdd835",
+                "opacity": 1
+            },
+            {
+                "color": "#fb8c00",
+                "opacity": 1
+            },
+            {
+                "color": "#8d6e63",
+                "opacity": 1
+            },
+            {
+                "color": "#9e9e9e",
+                "opacity": 1
+            },
+            {
+                "color": "#607d8b",
+                "opacity": 1
+            }
+        ],
+        "themeIncreaseColor": {
+            "color": "#388e3c",
+            "opacity": 1
+        },
+        "themeDecreaseColor": {
+            "color": "#f44336",
+            "opacity": 1
+        },
+        "themeGridColor": {
+            "color": "#e9e9e9",
+            "opacity": 1
+        }
+    },
+    "interactions": {}
+};
